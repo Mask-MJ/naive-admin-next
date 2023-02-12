6836dd4915fc186fa2f553939d6abfbbfc24d91c
mask <617473294@qq.com>
2023-02-12 18:56:22 +0800

    feat(router): 新增白名单 | 文件路由 | layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,10 @@
 <template>
-  <div> 404 </div>
+  <div>
+    layouts
+    <router-view />
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped></style>
-<route lang="yaml">
-meta:
-  noAuth: true
-</route>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,22 @@
 import type { App } from 'vue';
+import type { RouteRecordRaw } from 'vue-router';
 import { createRouter, createWebHashHistory } from 'vue-router';
+import { setupLayouts } from 'virtual:generated-layouts';
+import routes from '~pages';
+import { filter } from 'lodash-es';
+// 白名单 不需要登录的路由
+const WHITE_NAME_LIST = setupLayouts(
+  filter(routes, (route) => route.meta?.noAuth) as RouteRecordRaw[],
+);
 
 export const router = createRouter({
   history: createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH),
-  routes: [],
+  routes: WHITE_NAME_LIST,
   strict: true,
   scrollBehavior: () => ({ left: 0, top: 0 }),
 });
 
 export const setupRouter = async (app: App) => {
+  console.log(routes);
   app.use(router);
 };
