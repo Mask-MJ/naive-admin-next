11ecb7defa99aba6dbef71df9b06618525a3d4d6
mask <617473294@qq.com>
2023-03-23 16:52:41 +0800

    feat(table): 表格操作栏新增收起按钮

--- src/components/Table/components/TableAction.vue
+++ src/components/Table/components/TableAction.vue
@@ -29,10 +29,20 @@
         <template v-if="action.label">{{ action.label }}</template>
       </n-button>
     </template>
+    <n-dropdown
+      trigger="hover"
+      :options="getDropdownList"
+      v-if="dropDownActions && getDropdownList.length > 0"
+    >
+      <n-button text size="small" v-if="!$slots.more" class="ml-2">
+        <i class="i-ant-design:ellipsis-outlined"></i>
+      </n-button>
+    </n-dropdown>
   </div>
 </template>
 
 <script setup lang="ts" name="TableAction">
+  import type { DropdownOption } from 'naive-ui';
   import type { ActionItem } from '../types/tableAction';
   // import { Size, Type } from 'naive-ui/es/button/src/interface';
   // import type { TableActionType } from '../types/table';
@@ -44,6 +54,10 @@
       type: Array as PropType<ActionItem[]>,
       default: null,
     },
+    dropDownActions: {
+      type: Array as PropType<ActionItem[]>,
+      default: null,
+    },
     divider: { type: Boolean, default: true },
     outside: { type: Boolean, default: false },
     stopButtonPropagation: { type: Boolean, default: false },
@@ -52,7 +66,7 @@
   /** 根据权限展示 */
   // const hasPermission = (auth?: string | string[]) => true;
 
-  function isIfShow(action: ActionItem): boolean {
+  function isIfShow(action: ActionItem | DropdownOption): boolean {
     const ifShow = action.ifShow;
 
     let isIfShow = true;
@@ -80,6 +94,19 @@
           onNegativeClick: popConfirm?.onNegativeClick,
           enable: !!popConfirm,
         };
+      });
+  });
+
+  const getDropdownList = computed((): DropdownOption[] => {
+    return (toRaw(props.dropDownActions) || [])
+      .filter((dropDownActions) => isIfShow(dropDownActions))
+      .map((action) => {
+        return {
+          label: action.tooltip,
+          key: action.icon,
+          icon: () => h('i', { class: action.icon }),
+          props: { onClick: action?.onClick },
+        } as DropdownOption;
       });
   });
 
--- src/components/Table/index.ts
+++ src/components/Table/index.ts
@@ -1,4 +1,5 @@
 export * from './types/table';
+export * from './types/tableAction';
 
 export { default as TableAction } from './components/TableAction.vue';
 export { useTable } from './hooks/useTable';
